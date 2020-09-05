e73671cc128ce3f3a66016f7d850ce4261a97456
nithinkrishna1812 <37503357+nithinkrishna1812@users.noreply.github.com>
2020-09-05 20:13:36 +0530

    Update cart.js

--- mysite/static/js/cart.js
+++ mysite/static/js/cart.js
@@ -6,12 +6,33 @@ for(i=0; i < updateBtns.length; i++){
         var action = this.dataset.action
 
         if (user == 'AnonymousUser'){
-            console.log("not logged  in.....")
+            addCookieItem(productId, action)
         }
         else{
             updateUserOrder(productId, action)
         }
     });
+}
+
+function addCookieItem(productId, action) {
+    if (action == 'add') {
+        if (cart[productId] == undefined) {
+            cart[productId] = { 'quantity': 1 }
+        }
+        else{
+            cart[productId]['quantity'] += 1  
+        }        
+    }
+
+    if (action == 'remove') {
+        cart[productId]['quantity'] -= 1
+
+        if (cart[productId]['quantity'] <= 0) {
+            delete cart[productId]
+        }
+    }
+    document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/"
+    location.reload()
 }
 
 function updateUserOrder(productId, action){
@@ -29,7 +50,5 @@ function updateUserOrder(productId, action){
     })
     .then((data) => {
         location.reload()
-    });
-
-    
+    });    
 }
